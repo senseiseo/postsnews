6403c0c14d8b40ead3f32b0a6e67f6eb2cf77688
senseiseo <1919mur@gmail.com>
2020-07-01 18:23:11 +0300

    1

--- db/migrate/20200701020000_modify_topics.rb
+++ db/migrate/20200701020000_modify_topics.rb
@@ -2,10 +2,10 @@
 class ModifyTopics < ActiveRecord::Migration[6.0]
   def change
     add_index :topics, :alias, unique: true
-    Topic.create :alias => "Kino",     :title => "Кино"
-    Topic.create :alias => "Sport",    :title => "Спорт"
-    Topic.create :alias => "Politika", :title => "Политика"
-    Topic.create :alias => "IT",       :title => "ИТ новости"
-    Topic.create :alias => "Games",    :title => "Игры"
+    Topic.create :alias => "kino",     :title => "Кино"
+    Topic.create :alias => "sport",    :title => "Спорт"
+    Topic.create :alias => "politika", :title => "Политика"
+    Topic.create :alias => "it",       :title => "ИТ новости"
+    Topic.create :alias => "games",    :title => "Игры"
   end
 end
